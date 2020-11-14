41813588076e6871ba49aaaf6d3942094752733e
Kevin Novak <kevinmichaelnovak@gmail.com>
2020-11-13 21:59:15 -0500

    Move plugin name to const

--- core/src/main/kotlin/me/kevinnovak/nms_kotlin_template/services/Logger.kt
+++ core/src/main/kotlin/me/kevinnovak/nms_kotlin_template/services/Logger.kt
@@ -3,18 +3,20 @@ package me.kevinnovak.nms_kotlin_template.services
 import org.bukkit.Bukkit
 import java.lang.Exception
 
+const val PLUGIN_NAME = "NmsKotlinTutorial"
+
 class Logger {
     companion object {
         fun info(message: String) {
-            Bukkit.getLogger().info("[NmsKotlinTutorial] $message")
+            Bukkit.getLogger().info("[$PLUGIN_NAME] $message")
         }
 
         fun warn(message: String) {
-            Bukkit.getLogger().warning("[NmsKotlinTutorial] $message")
+            Bukkit.getLogger().warning("[$PLUGIN_NAME] $message")
         }
 
         fun error(message: String, ex: Exception? = null) {
-            Bukkit.getLogger().severe("[NmsKotlinTutorial] $message")
+            Bukkit.getLogger().severe("[$PLUGIN_NAME] $message")
 
             if (ex == null) {
                 return
