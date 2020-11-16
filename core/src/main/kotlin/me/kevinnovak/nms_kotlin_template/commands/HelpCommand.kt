3646891cf48863baf5cd2f411053e04a6a481cb5
Kevin Novak <kevinmichaelnovak@gmail.com>
2020-11-15 19:55:00 -0500

    Show help command in help

--- core/src/main/kotlin/me/kevinnovak/nms_kotlin_template/commands/HelpCommand.kt
+++ core/src/main/kotlin/me/kevinnovak/nms_kotlin_template/commands/HelpCommand.kt
@@ -6,17 +6,22 @@ import org.bukkit.entity.Player
 class HelpCommand(private var commands: List<Command>) : Command {
     override var name = "help"
     override var aliases = listOf("?")
-    override var usage: String = "/test help"
+    override var usage: String = "/test"
     override var description: String = "Shows plugin help."
 
     override fun execute(player: Player, args: Array<String>) {
         var lines = mutableListOf("&e-------- &6NMS Kotlin Template &e- &6Commands &e--------")
+        lines.add(formatLine(this))
         for (command in this.commands) {
             if (player.hasPermission("nmskotlintemplate.commands.${command.name}")) {
-                lines.add("&b${command.usage} &7- ${command.description}")
+                lines.add(formatLine(command))
             }
         }
         var msg = lines.joinToString("\n")
         MessageService.send(player, msg)
+    }
+
+    private fun formatLine(command: Command): String {
+        return "&b${command.usage} &7- ${command.description}"
     }
 }
