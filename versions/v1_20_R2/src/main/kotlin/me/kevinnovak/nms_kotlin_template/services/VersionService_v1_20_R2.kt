0ee5fd53c6bbb8eb21b9f05a25331bacd0edc56f
Kevin Novak <kevinmichaelnovak@gmail.com>
2024-02-29 21:20:49 -0500

    Fix typo version number

--- versions/v1_20_R2/src/main/kotlin/me/kevinnovak/nms_kotlin_template/services/VersionService_v1_20_R2.kt
+++ versions/v1_20_R2/src/main/kotlin/me/kevinnovak/nms_kotlin_template/services/VersionService_v1_20_R2.kt
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.ChatMessageType
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.entity.Player
 
-class VersionService_v1_20_R3 : VersionService {
+class VersionService_v1_20_R2 : VersionService {
     override fun sendActionBar(player: Player, message: String) {
         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent(message))
     }
